c0a3197b6aba208e51b9a9d7d88671a9acd0522a
jacobdshimer <31100766+jacobdshimer@users.noreply.github.com>
2017-11-17 23:34:16 -0500

    Update Bro.py
    Bro.py was recreating the combined_ file over and over again.  It also was rewriting a file multiple times even if it was only created once.  This was fixed by deleting it after each time bro is ran.

--- bro.py
+++ bro.py
@@ -14,11 +14,36 @@ def bro(verbose, files=[]):
 			subprocess.check_output(["bro","-r",file])
 			newfiles = subprocess.check_output(["ls"]).split()
 			for newfile in newfiles:
-				combinedfile = open("combined_" + str(newfile),"a+")
-				newfile = open(newfile,"r")
-				combinedfile.write(newfile)
-				combinedfile.close()
-				newfile.close()
+				if "combined_" not in newfile:	
+					combinedfile = open("combined_" + str(newfile),"a+")
+					newfile = open(newfile,"r")
+					data = newfile.read()
+					combinedfile.write(data)
+					name = str(newfile.split()
+					name = name[2].strip(",").strip("'")
+					combinedfile.close()
+					newfile.close()
+					os.remove(path+"/"+str(epoch)+"/"+str(name))
+				else:
+					pass
+						   
+		allcombined = subprocess.check_output(["ls"]).split()
+		for all in allcombined:
+			count = 1
+			data = open(all,"r")
+			lines = data.readlines()
+			data.close()
+			data = open(all,"w")
+			for line in lines:
+				if "#" in line:
+					if count==7 or count==8:
+						data.write(line)
+						count+=1
+					else:
+						count+=1
+						pass
+				else:
+					data.write(line)
 				
 	elif verbose == True:
 		count = 1
@@ -27,14 +52,44 @@ def bro(verbose, files=[]):
 		os.mkdir(path + "/" + str(epoch))
 		os.chdir(path + "/" + str(epoch))
 		print "Creating the folder " + str(epoch) + "in order to store Bro Logs safely."
+		print "Preparing to run " +str(len(files)) + " against the Bro IDS."
 		for file in files:
 			print "Working on " + str(count) + " out of " + str(len(files))
 			subprocess.check_output(["bro","-r",file])
 			newfiles = subprocess.check_output(["ls"]).split()
 			for newfile in newfiles:
-				combinedfile = open("combined_" + str(newfile),"a+")
-				newfile = open(newfile,"r")
-				combinedfile.write(newfile)
-				combinedfile.close()
-				newfile.close()
-			count += 1
+				if "combined_" not in newfile:	
+					combinedfile = open("combined_" + str(newfile),"a+")
+					newfile = open(newfile,"r")
+					data = newfile.read()
+					combinedfile.write(data)
+					name = str(newfile.split()
+					name = name[2].strip(",").strip("'")
+					combinedfile.close()
+					newfile.close()
+					os.remove(path+"/"+str(epoch)+"/"+str(name))
+				else:
+					pass
+			count +=1
+						   
+		allcombined = subprocess.check_output(["ls"]).split()
+		print "Fixing up the combined files to have only one header!"
+		filecount = 1
+		for all in allcombined:
+			print "Working on " + str(filecount) + " out of " + str(len(allcombined))
+			count = 1
+			data = open(all,"r")
+			lines = data.readlines()
+			data.close()
+			data = open(all,"w")
+			for line in lines:
+				if "#" in line:
+					if count==7 or count==8:
+						data.write(line)
+						count+=1
+					else:
+						count+=1
+						pass
+				else:
+					data.write(line)
+			filecount+=1
